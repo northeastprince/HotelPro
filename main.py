18bd12eba9803f322d145f376f9c71262a10658d
Matty Almeida <55164724+robotmaster101@users.noreply.github.com>
2020-02-25 19:01:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,82 @@
 import time
 
+import json
+
+
+
 time.sleep(3)
-print("Welcome to the PMS!")
+
+print('Welcome to the PMS!')
+
 time.sleep(2)
+
 auth = False
+
 terminal = True
-#User Verifacation
+
+
+
+id = raw_input('Enter User ID: ')
+
+if id == '519':
+
+    auth = True
+
+else: auth = False
+
+
 
 while auth == False:
-    id = raw_input("Enter User ID: ")
+
+    print('Invalid credentials')
+
+    id = raw_input('Enter User ID: ')
+
     if id == '519':
+
         auth = True
-    elif id == '769':
-        auth = True
-else: auth = False
+
+    else: auth = False
 
 current_user = id
 
-room_status = ['R1 Busy', 'R2 Busy', 'R3 Busy', 'R4 Avalible']
 
-#Command process
 
-while terminal == True:
-    entered_command = raw_input('Terminal: ')
+#Command Process
+
+
+
+filename = 'room_status.json'
+
+with open(filename) as f_obj:
+  room_status = json.load(f_obj)
+
+
+
+while auth == True:
+
+    entered_command = raw_input()
+
     if entered_command == 'room_status':
+
         print(room_status)
+
     elif entered_command == 'current_user':
-        print('The current user is: ' + id)
+
+        print('Current User Is: ' + current_user)
+
     elif entered_command == 'add_room':
+
         add = raw_input('Enter Room Value: ')
+
         room_status.append(add)
-    elif entered_command == 'customer_edit':
-        print(customer_edit)
-        cus_edit = raw_input('Enter Full New Value: ')
-        customer_edit = cus_edit
-        print(customer_edit)
+
+    elif entered_command == 'book_room':
+
+        cus_num = raw_input('Enter Customer Number')
+
+        room_num = raw_input('Enter Room Num without prefix: ')
+
+        room_status[int(room_num) - 1] = 'R' + room_num + ' Booked to: ' + cus_num
+
+    else: print ('Invalid Command')
